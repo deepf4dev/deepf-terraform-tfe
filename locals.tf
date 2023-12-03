dab50670615b83556fc39006af6203935c93bec9
deepf4dev <deepf4dev@gmail.com>
2023-12-03 20:25:13 +0100

    feat: setup github workspace

--- locals.tf
+++ locals.tf
@@ -16,7 +16,7 @@ locals {
 
     "deepf-github" = {
       description         = "Automation workspace for GitHub resources"
-      execution_mode      = "remote"
+      execution_mode      = "local"
       project_id          = module.project["deepf-project"].id
       vcs_repo_identifier = "${var.github_organization_name}/deepf-terraform-github"
     }
